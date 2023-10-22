6500252e2267ba0ba345d25de0490282329a7151
Dmytro Sayko <saykodmytro@icloud.com>
2023-10-22 15:06:20 +0300

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,24 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
 
-console.log('dfvdv');
+import { Container } from './Container/Container';
+import PhoneForm from './PhoneForm/PhoneForm';
+import { Title } from './Title/Title';
+
+export class App extends Component {
+  render() {
+    return (
+      <>
+        <Container>
+          <Title title="Phonebook" />
+          <PhoneForm />
+        </Container>
+        <Container>
+          <Title title="Search" />
+        </Container>
+        <Container>
+          <Title title="Contacts" />
+        </Container>
+      </>
+    );
+  }
+}
